e00515a4e4fee113575be92b084f54eb25b0b07e
Andrei <krestonosa@gmail.com>
2025-03-31 20:52:21 +0200

    reading documentation

---
+++ src/server.cpp
@@ -0,0 +1,19 @@
+#include "webserv.hpp"
+#include <iostream>
+
+   
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int main()
+{
+    using namespace http;
+    TcpServer server = TcpServer();
+
+    int socket(int domain, int type, int protocol);
+
+
+    return 0;
+}
