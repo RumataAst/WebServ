2352bb7fe83795922b7408cdc5d9c08bc304d9c6
Andrei <krestonosa@gmail.com>
2025-04-04 20:54:34 +0200

    Creating simple abstract Socket class that will be inhereted by Server and client. Adding test_connection function, currently if connection fails the program terminates with EXIT_FAILURE status which is not ideal

--- src/Socket/socket.hpp
+++ src/Socket/socket.hpp
@@ -1,12 +1,16 @@
-#include SOCKET_HPP
+#ifndef SOCKET_HPP
 #define SOCKET_HPP
 
+#include <iostream>     //std::cerr 
 #include <stdio.h>
 #include <sys/socket.h> // for socket function
 #include <netinet/in.h> // for address struct
 
 namespace hde {
     class SimpleSocket{
+        private:
+            int     sock_return, connection;
+            struct  sockaddr_in address;
         public:
         /* Default constructor requires from user domain, type and protocol more info in socket function
         domain specifies the communication domain in which a socket to be created. 
@@ -15,11 +19,14 @@ namespace hde {
         port for Network
         interface on which socket is running
         */
-            SimpleSocket(int domain, int type, int protocol, int port, u_long interface);
-            virtual void create_network_connection(int sock_return,struct sockaddr_in address) = 0;
-        private:
-            int     sock_return, connection;
-            struct  sockaddr_in_address;
+        SimpleSocket(int domain, int type, int protocol, int port, u_long interface);
+        void   test_connection(int item_to_test);
+        
+        int get_socket () const;
+        int get_connection () const;
+        struct  sockaddr_in get_address() const;
+        
+        virtual int create_network_connection(int &sock_return, struct sockaddr_in &address) = 0;
     };
 }
 
