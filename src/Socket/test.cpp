8ed1e7e8ff6467c86fc45c8acffc4bb05f6a1297
Andrei <krestonosa@gmail.com>
2025-04-27 21:44:28 +0200

    deleting client thread and just connectign via telnet to the localhost ip 127.0.0.1 that is routed directly within the operation system's networking stack and same port in this case 12345 + adding more messages. Plans Create just 1 class socket, change sockets statuses to nonblocking and also add function setsockopt to force bind the socket to avoid Address already in use error.

--- src/Socket/test.cpp
+++ src/Socket/test.cpp
@@ -13,45 +13,30 @@ void server_thread() {
     int client_fd = listen_socket.accept_connection(client_addr);
     std::cout << "Server accepted connection from client\n";
 
-    char buffer[1024] = {};
-    int bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
-    if (bytes_received > 0) {
-        std::cout << "Server received: " << buffer << std::endl;
+    char buffer[1024];
 
-        const char *reply = "Hello from server";
-        send(client_fd, reply, strlen(reply), 0);
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));
+        int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+
+        if (bytes_received > 0) {
+            std::cout << "Server received: " << buffer << std::endl;
+        } else if (bytes_received == 0) {
+            std::cout << "Client disconnected\n";
+            break;
+        } else {
+            std::cerr << "recv() error\n";
+            break;
+        }
     }
 
     close(client_fd);
 }
 
-void client_thread() {
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    net::ConnectingSocket connect_socket(AF_INET, SOCK_STREAM, 0, 12345, INADDR_LOOPBACK);
-
-    int sockfd = connect_socket.get_socket();
-
-    const char *msg = "Hello from client";
-    send(sockfd, msg, strlen(msg), 0);
-    std::cout << "Client sent message\n";
-
-    char buffer[1024] = {};
-    int bytes_received = recv(sockfd, buffer, sizeof(buffer), 0);
-    if (bytes_received > 0) {
-        std::cout << "Client received: " << buffer << std::endl;
-    }
-
-    close(sockfd);
-}
-
 int main() {
     try {
         std::thread server(server_thread);
-        std::thread client(client_thread);
-
         server.join();
-        client.join();
-
         std::cout << "End of test\n";
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
